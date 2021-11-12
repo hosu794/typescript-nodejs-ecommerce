599c27dbf6a2f05e6a742045ac38aa30045906f4
hosu794 <grzesszczesny14@gmail.com>
2021-11-12 18:13:22 +0100

    Finish a category controller, but needs jwt roles to works :)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 
 import AuthenticationController from './authentication/authentication.controller';
+import CategoryController from './category/category.controller';
 import PaypalController from './paypal/paypal.controller';
 import App from './server'
 import UserController from './user/user.controller';
 
-const Application = new App([new PaypalController, new UserController, new AuthenticationController]);
+const Application = new App([new PaypalController, new UserController, new AuthenticationController, new CategoryController]);
 
 
--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,10 +2,55 @@ import { Router } from "express";
 import Controller from "../interfaces/controller.interface";
 
 import express from 'express'; 
+import DatabaseConnection from "../config/database";
+
+import Pool from 'pg'
 
 class CategoryController implements Controller {
     public readonly path: string = '/categories'; 
     router: Router = express.Router();
-    
+
+    database: any;
+
+    constructor() {
+        this.database = new DatabaseConnection().getDB(); 
+    }
+
+    private initializeControllers() {
+        this.router.get(`${this.path}`, this.getAllCategories); 
+        this.router.get(`${this.path}/:id`, this.getCategoryById); 
+    }
+
+    getAllCategories = async (request: express.Request, response: express.Response) => {
+
+    const sqlQuery: string = "SELECT * FROM categories"; 
+
+    const { rows } = await this.database.query(sqlQuery); 
+
+    if(rows) {
+        response.sendStatus(200).json({
+            categories: rows
+        })
+    } else {
+
+        response.sendStatus(401).json({message: 'Categories not found!'}); 
+
+    }
+
+    }
+
+    getCategoryById = async (request: express.Request, response: express.Response) => {
+
+        const userId: number = Number(request.params.id); 
+
+        const sqlQuery: string = "SELECT * FROM categories WHERE category_id = $1"; 
+
+        const result  = await this.database(sqlQuery, [userId]);
+
+        console.log(result); 
+
+    }
 
 }
+
+export default CategoryController; 
