8a80fd3ebbf86357d700fef7b4c7776a7b158a17
hosu794 <grzesszczesny14@gmail.com>
2021-12-01 18:17:52 +0100

    Create a payment create function.

---
+++ src/payment/payment.controller.ts
@@ -0,0 +1,108 @@
+import express from 'express'; 
+import { QueryResult } from 'pg';
+import Controller from "../interfaces/controller.interface";
+import Pool from 'pg'; 
+import DatabaseConnection from '../config/database';
+import paypal, { Item, order, Payment, payment as PaypalPayment } from 'paypal-rest-sdk'
+
+import { ProductResponse } from '../product/product.interfaces'; 
+
+class PaymentController implements Controller {
+    public readonly path: string = "/payments"; 
+    public router: express.Router = express.Router(); 
+    database: Pool.Pool;
+
+    constructor() {
+        this.initializeRoutes();
+        this.database = new DatabaseConnection().getDB(); 
+    }
+
+    private initializeRoutes() {
+
+        this.router.post(`${this.path}/order/:id`, this.createPayment); 
+
+    }
+
+    private createPayment = async (request: express.Request, response: express.Response) => {
+
+        const orderId: number = Number(request.params.id); 
+
+        const sqlToGetOrderById: string = "SELECT * FROM orders WHERE order_id = $1"; 
+        
+        const resultOfOrder: QueryResult = await this.database.query(sqlToGetOrderById, [orderId]);
+        
+        if(resultOfOrder.rowCount === 0) response.status(404).json({message: "Order not found!"}); 
+
+        const sqlToGetAllProducts: string = "SELECT * FROM products JOIN order_product on products.product_id = order_product.product_id WHERE order_id = $1";
+        
+        console.log('Order id:', orderId);
+
+        const resultOfProducts: QueryResult = await this.database.query(sqlToGetAllProducts, [orderId]);
+        
+        console.log(resultOfProducts); 
+
+        let totalValueToPay: number = 0; 
+
+        resultOfProducts.rows.map((item: ProductResponse) =>  {
+            totalValueToPay = totalValueToPay + Number(item.price); 
+        }); 
+
+        console.log(`Total value: ${totalValueToPay}`);
+
+        const create_payment: Payment = {
+            intent: 'sale', 
+            payer: {
+                payment_method: 'paypal'
+            },
+            redirect_urls: {
+                return_url: "http://localhost:3000/success", 
+                cancel_url: "http://localhost:300/cancel"
+            },
+            transactions: [{
+                amount: {
+                    currency: "USD", 
+                    total: String(totalValueToPay)
+                },
+                description: "Current Case"
+            }]
+
+        }
+
+        paypal.payment.create(create_payment, (error: any, payment: any) => {
+
+            if (error) {
+                throw error;
+            } else {
+                for(let i = 0;i < payment.links.length;i++){
+                  if(payment.links[i].rel === 'approval_url'){
+                    response.redirect(payment.links[i].href);
+                  }
+                }
+            }
+        })
+
+
+    }
+
+    private cancelPayment = async (request: express.Request, response: express.Response) => {
+        response.send("Cancelled!"); 
+    }
+
+    private checkIsCaseExistsFromUser = async (request: express.Request, response: express.Response) => {
+
+        //@ts-ignore
+        const currentUserId: number = Number(request.user.id); 
+
+        const sqlToCheckIsUserExists: string = "SELECT * FROM cases WHERE user_id = $1"; 
+
+        const result: QueryResult = await this.database.query(sqlToCheckIsUserExists, [currentUserId]); 
+
+        if(result.rowCount === 0) return false;
+        else return true;
+
+    }
+
+
+}
+
+export default PaymentController; 
